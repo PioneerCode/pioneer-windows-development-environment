1f75c30be1aef151e60ae3f585e73c3932aff360
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-04-05 11:30:08 -0500

    Updated meta tasks

--- scripts/meta-tasks.ps1
+++ scripts/meta-tasks.ps1
@@ -1,7 +1,8 @@
 #################################################################################################
-# Random tasks
-#   - Used more as a reminder then anything
+# Random tasks - Used more as a reminder then anything
+#   1) Change values in git config section
 #################################################################################################
-
+write-host "Updating git config"
 git config --global user.email "your_email@example.com"
 git config --global user.name "Billy Everyteen"
+write-host "End Updating Git Config . . ."
