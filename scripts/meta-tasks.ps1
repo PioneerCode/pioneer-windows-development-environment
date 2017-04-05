c949039ba7ff43291c7a5b062ae14571b81f086d
Chad Ramos <Chad Ramos>
2017-04-05 11:26:24 -0500

    Added meta tasks

---
+++ scripts/meta-tasks.ps1
@@ -0,0 +1,7 @@
+#################################################################################################
+# Random tasks
+#   - Used more as a reminder then anything
+#################################################################################################
+
+git config --global user.email "your_email@example.com"
+git config --global user.name "Billy Everyteen"
