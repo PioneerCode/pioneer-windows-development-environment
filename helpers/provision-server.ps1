cb0304333fc378f2b00ca86368388e670bb7ba95
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-04-23 19:52:01 -0500

    Server setup hleper

---
+++ helpers/provision-server.ps1
@@ -0,0 +1,54 @@
+########################################################################################################
+# Base installs for Pioneer server
+#   1) Install Chocolatey 
+#   2) Install Chocolatey packages - Additonal packages can be found at https://chocolatey.org/package
+#   
+########################################################################################################
+
+# Choco Install
+Write-host "Choco Started At: $((Get-Date).ToString())"
+
+$ChocoInstallPath = "$($env:SystemDrive)\ProgramData\Chocolatey\bin"
+if (!(Test-Path $ChocoInstallPath)) {
+    write-host "Install Chocolatey . . . "
+    Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) | out-null
+    write-host "END Installing Chocolatey!" 
+} else {
+    write-host "Upgrade Chocolatey . . . "
+    choco upgrade chocolatey
+    write-host "END Upgrade Chocolatey!"
+}
+
+chocolatey feature enable -n=allowGlobalConfirmation
+
+write-host "Install WinRAR . . . "
+cinst -y  winrar | Out-Null
+write-host "END Install WinRAR!"
+
+write-host "Install Dropbox . . . "
+cinst -y  dropbox | Out-Null
+write-host "END Install Dropbox!"
+
+write-host "Install Octopus Deploy . . . "
+cinst -y  octopusdeploy  | Out-Null
+write-host "END Install Octopus Deploy!"
+
+write-host "Install Octopus Deploy Tentacle . . . "
+cinst -y  octopusdeploy.tentacle | Out-Null
+write-host "END Install Octopus Deploy Tentacle!"
+
+write-host "Install TeamCity . . . "
+cinst -y  teamcity | Out-Null
+write-host "END Install TeamCity!"
+
+write-host "Install Plex Media Server . . . "
+cinst -y  plexmediaserver | Out-Null
+write-host "END Install Plex Media Server!"
+
+write-host "Install SQL Managment Studio . . . "
+cinst -y  sql-server-management-studio | Out-Null
+write-host "END Install SQL Managment Studio!"
+
+chocolatey feature disable -n=allowGlobalConfirmation
+
+Write-host "Choco Ended At: $((Get-Date).ToString())"
